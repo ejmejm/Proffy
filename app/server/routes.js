228ee81de89fd50ebf94d8176a281eea9eca5451
ejmejm <ninjaedan@aol.com>
2016-11-12 11:51:58 -0800

    adding stuff

--- app/server/routes.js
+++ app/server/routes.js
@@ -22,7 +22,7 @@ module.exports = function(app) {
 			});
 		}
 	});
-	
+
 	app.post('/', function(req, res){
 		AM.manualLogin(req.body['user'], req.body['pass'], function(e, o){
 			if (!o){
@@ -37,22 +37,26 @@ module.exports = function(app) {
 			}
 		});
 	});
-	
+
 // logged-in user homepage //
-	
+
 	app.get('/home', function(req, res) {
 		if (req.session.user == null){
 	// if user is not logged-in redirect back to login page //
 			res.redirect('/');
-		}	else{
+		}else if(req.session.user.firstLogin == 'false'){
 			res.render('home', {
 				title : 'Control Panel',
 				countries : CT,
 				udata : req.session.user
 			});
+		}else{
+			res.render('firstLogin', {
+				title: 'Proffy',
+			});
 		}
 	});
-	
+
 	app.post('/home', function(req, res){
 		if (req.session.user == null){
 			res.redirect('/');
@@ -71,7 +75,7 @@ module.exports = function(app) {
 			// update the user's login cookies if they exists //
 					if (req.cookies.user != undefined && req.cookies.pass != undefined){
 						res.cookie('user', o.user, { maxAge: 900000 });
-						res.cookie('pass', o.pass, { maxAge: 900000 });	
+						res.cookie('pass', o.pass, { maxAge: 900000 });
 					}
 					res.status(200).send('ok');
 				}
@@ -84,20 +88,21 @@ module.exports = function(app) {
 		res.clearCookie('pass');
 		req.session.destroy(function(e){ res.status(200).send('ok'); });
 	})
-	
+
 // creating new accounts //
-	
+
 	app.get('/signup', function(req, res) {
 		res.render('signup', {  title: 'Signup', countries : CT });
 	});
-	
+
 	app.post('/signup', function(req, res){
 		AM.addNewAccount({
 			name 	: req.body['name'],
 			email 	: req.body['email'],
 			user 	: req.body['user'],
 			pass	: req.body['pass'],
-			country : req.body['country']
+			country : req.body['country'],
+			firstLogin: 'true'
 		}, function(e){
 			if (e){
 				res.status(400).send(e);
@@ -106,6 +111,20 @@ module.exports = function(app) {
 			}
 		});
 	});
+
+// User Settings //
+
+app.get('/settings', function(req, res) {
+	if (req.session.user == null){
+// if user is not logged-in redirect back to login page //
+		res.redirect('/');
+	}else{
+		res.render('settings', {
+			title : 'User Settings',
+			udata : req.session.user
+		});
+	}
+});
 
 // password reset //
 
@@ -142,7 +161,7 @@ module.exports = function(app) {
 			}
 		})
 	});
-	
+
 	app.post('/reset-password', function(req, res) {
 		var nPass = req.body['pass'];
 	// retrieve the user's email from the session to lookup their account and reset password //
@@ -157,15 +176,15 @@ module.exports = function(app) {
 			}
 		})
 	});
-	
+
 // view & delete accounts //
-	
+
 	app.get('/print', function(req, res) {
 		AM.getAllRecords( function(e, accounts){
 			res.render('print', { title : 'Account List', accts : accounts });
 		})
 	});
-	
+
 	app.post('/delete', function(req, res){
 		AM.deleteAccount(req.body.id, function(e, obj){
 			if (!e){
@@ -177,13 +196,13 @@ module.exports = function(app) {
 			}
 	    });
 	});
-	
+
 	app.get('/reset', function(req, res) {
 		AM.delAllRecords(function(){
-			res.redirect('/print');	
+			res.redirect('/print');
 		});
 	});
-	
+
 	app.get('*', function(req, res) { res.render('404', { title: 'Page Not Found'}); });
 
 };
